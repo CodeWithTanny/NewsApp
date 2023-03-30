79e52f2f2994da4074e7400d64619f6f488e0da9
Tanmay Sahare <tanmaysahare2001@gmail.com>
2023-03-30 17:38:46 +0530

    password authenication done with jsonwebtoken

--- inotebook/Backend/routers/auth.js
+++ inotebook/Backend/routers/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs'); //for convering password into secure password we have install theses pacckage
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Tanmayisagoodboy";
 
 
 //create a user using: POST "api/auth/createuser". Doesn't require Auth. No login required
@@ -22,13 +26,26 @@ router.post('/createuser',[ //these can be taken from express validator
     if(user){
       return res.status(400).json({error: "Sorry a user with this email alreday exists"})
     }
+    const salt = await bcrypt.genSalt(10); // these two line is used for password hasing
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
     //create a new user
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-    })
-    res.json(user)
+      password: secPass,
+    });
+
+  const data = {
+    usser:{
+      id: user.id
+    }
+    }
+  const authtoken = jwt.sign(data, JWT_SECRET);
+  
+    
+    res.json(authtoken)
+
   } catch(error){
     console.error(error.message);
     res.status(500).send("some error occured");
